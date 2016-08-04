25bc4265738106fc7be23335196177b18868cca9
Edouard DUPIN <yui.heero@gmail.com>
2016-08-04 23:27:25 +0200

    [DEV] integrate ebml 1.3.4

---
+++ lutin_ebml.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+import lutin.module as module
+import lutin.tools as tools
+
+
+def get_type():
+	return "LIBRARY"
+
+def get_desc():
+	return "ebml library"
+
+def get_licence():
+	return "LGPL-2.1"
+
+def get_maintainer():
+	return ["Moritz Bunkus <moritz@bunkus.org>"]
+
+def get_version():
+	return [1,3,4]
+
+def create(target, module_name):
+	my_module = module.Module(__file__, module_name, get_type())
+	my_module.add_src_file([
+	    'ebml/src/EbmlHead.cpp',
+	    'ebml/src/EbmlCrc32.cpp',
+	    'ebml/src/EbmlString.cpp',
+	    'ebml/src/MemIOCallback.cpp',
+	    'ebml/src/EbmlBinary.cpp',
+	    'ebml/src/EbmlStream.cpp',
+	    'ebml/src/EbmlSubHead.cpp',
+	    'ebml/src/EbmlFloat.cpp',
+	    'ebml/src/EbmlVersion.cpp',
+	    'ebml/src/MemReadIOCallback.cpp',
+	    'ebml/src/EbmlDate.cpp',
+	    'ebml/src/IOCallback.cpp',
+	    'ebml/src/Debug.cpp',
+	    'ebml/src/EbmlUInteger.cpp',
+	    'ebml/src/EbmlVoid.cpp',
+	    'ebml/src/EbmlSInteger.cpp',
+	    'ebml/src/EbmlContexts.cpp',
+	    'ebml/src/EbmlDummy.cpp',
+	    'ebml/src/StdIOCallback.cpp',
+	    'ebml/src/EbmlUnicodeString.cpp',
+	    'ebml/src/SafeReadIOCallback.cpp',
+	    'ebml/src/EbmlMaster.cpp',
+	    'ebml/src/EbmlElement.cpp',
+		])
+	my_module.add_header_file([
+	    'ebml/ebml/EbmlMaster.h',
+	    'ebml/ebml/EbmlHead.h',
+	    'ebml/ebml/EbmlBinary.h',
+	    'ebml/ebml/EbmlSubHead.h',
+	    'ebml/ebml/EbmlString.h',
+	    'ebml/ebml/IOCallback.h',
+	    'ebml/ebml/EbmlStream.h',
+	    'ebml/ebml/EbmlDate.h',
+	    'ebml/ebml/EbmlContexts.h',
+	    'ebml/ebml/EbmlEndian.h',
+	    'ebml/ebml/Debug.h',
+	    'ebml/ebml/EbmlCrc32.h',
+	    'ebml/ebml/EbmlTypes.h',
+	    'ebml/ebml/EbmlUnicodeString.h',
+	    'ebml/ebml/MemReadIOCallback.h',
+	    'ebml/ebml/EbmlConfig.h',
+	    'ebml/ebml/EbmlFloat.h',
+	    'ebml/ebml/MemIOCallback.h',
+	    'ebml/ebml/EbmlSInteger.h',
+	    'ebml/ebml/EbmlElement.h',
+	    'ebml/ebml/EbmlDummy.h',
+	    'ebml/ebml/StdIOCallback.h',
+	    'ebml/ebml/EbmlUInteger.h',
+	    'ebml/ebml/EbmlVoid.h',
+	    'ebml/ebml/EbmlId.h',
+	    'ebml/ebml/SafeReadIOCallback.h',
+	    'ebml/ebml/EbmlVersion.h',
+		],
+		destination_path="ebml")
+	my_module.add_header_file([
+	    'ebml/ebml/c/libebml_t.h',
+		],
+		destination_path="ebml/c")
+	my_module.add_module_depend([
+	    'cxx'
+	    ])
+	my_module.compile_version("C++", 2003)
+	return my_module
+
+
